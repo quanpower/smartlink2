85cac590453cc026f85dafac7b95a67f53168300
quanpower <quanpower@gmail.com>
2018-04-21 11:34:43 +0800

    add configure files

---
+++ smartlink2/gunicorn.conf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Author:William Zhang
+# Email:quanpower@gmail.com
+# Wechat:252527676
+# Created:18-4-21 下午9:13
+
+command = '/root/ENV/smartlink2/bin/gunicorn'
+pythonpath = '/root/ENV/smartlink2/bin/'
+bind = 'unix:/tmp/gunicorn.sock'
+# bind = '0.0.0.0:8000'
+workers = 5
+user = 'root'
